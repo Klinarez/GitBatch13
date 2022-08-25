172764e544a08a11182ab0637f0ae85d8d2bcfbe
klinarez <dominguezkarla2413@gmail.com>
2022-08-24 21:32:55 -0400

    added git ignore file

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,15 @@
+package class01;
+
+public class gitBasics {
+
+    public static void main(String[] args) {
+        //write down that checks the temperature from a website and then prints
+        //out that is a beautiful day or no
+        System.out.println("today is a beautiful day ");
+
+
+    }
+
+
+
+}
